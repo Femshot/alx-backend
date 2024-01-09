f834d1da76cd6da976132e5174685b38d3f99a8e
Awodele Oluwafemi <awodelefemi@yahoo.com>
2024-01-09 20:20:22 +0100

    Corrected pycodestyle comment syntax error

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -7,12 +7,12 @@ from flask_babel import Babel
 class Config(object):
     """ Configuration class for languages """
     LANGUAGES = ["en", "fr"]
-    #BABEL_DEFAULT_LOCALE = 'en'
-    #BABEL_DEFAULT_TIMEZONE = 'UTC'
+    # BABEL_DEFAULT_LOCALE = 'en'
+    # BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app = Flask(__name__)
-#app.config.from_object(Config)
+# app.config.from_object(Config)
 babel = Babel(app)
 
 
