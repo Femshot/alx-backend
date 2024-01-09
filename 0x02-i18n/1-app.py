83b2581ed9adddedb6b47afa06f30c8d123a9439
Awodele Oluwafemi <awodelefemi@yahoo.com>
2024-01-09 20:17:43 +0100

    Corrected index to render from 1-index.hmtl

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,19 +3,23 @@
 from flask import Flask, render_template
 from flask_babel import Babel
 
-app = Flask(__name__)
-babel = Babel(app)
-
 
 class Config(object):
     """ Configuration class for languages """
     LANGUAGES = ["en", "fr"]
+    #BABEL_DEFAULT_LOCALE = 'en'
+    #BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+#app.config.from_object(Config)
+babel = Babel(app)
 
 
 @app.route('/')
 def index():
     """ Index Page Route """
-    return render_template("0-index.html")
+    return render_template("1-index.html")
 
 
 if __name__ == '__main__':
